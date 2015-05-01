0b438c1c830920f5800a80072f6515f85d35b448
Tiffany Chen <yuhan210@gmail.com>
2015-05-01 15:48:41 -0400

    downloading director html pages

--- director-parser.py
+++ director-parser.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import imdb
+import os
+
 '''
 This script searches the box-office earning histories for each director
 '''
@@ -9,6 +11,22 @@ This script searches the box-office earning histories for each director
 def getMostRelevantSeach(movie_title, search_results):
     #TODO: use some heuristics to find the best match. Currently returns the first result
     return search_results[0]
+
+# Download director imdb webpage based on director_id
+def wgetDirectorIMDBPage(dir_id):
+    pwd = os.getcwd()
+    dir_o_folder = os.path.join(pwd, 'director_pages')
+    if not os.path.exists(dir_o_folder):
+            os.makedirs(dir_o_folder)
+
+    url = "http://www.imdb.com/name/nm"
+    webpage_path = os.path.join(dir_o_folder, dir_id)
+    print webpage_path
+    os.system('wget %s%s -O %s -q' %(url, dir_id, webpage_path))
+   # parseDirectorPage(os.path.join(dir_o_folder, dir_id))    
+
+# Parse director webpage
+#def parseDirectorPage(webpage_path):
 
 
 def getDirectorBoxEarningHistories(movie_title):
@@ -24,9 +42,9 @@ def getDirectorBoxEarningHistories(movie_title):
         dir_id = director.getID()
         
         # wget director imdb page
-
+        #http://www.imdb.com/name/nm0000142/
         print dir_id
-         
+        wgetDirectorIMDBPage(dir_id)     
     else:
         return None
 
